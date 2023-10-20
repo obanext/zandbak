c96c6bb3055216452f6ae5e850bb370d2f2ab4d1
obanext <93115438+obanext@users.noreply.github.com>
2023-10-20 15:03:28 +0200

    Update leefenleerkaart.js

--- leefenleerkaart.js
+++ leefenleerkaart.js
@@ -1,41 +1,72 @@
-function updateMap() {
-    var geonaamCheckboxes = document.querySelectorAll('.geonaam:checked');
-    var activiteitCheckboxes = document.querySelectorAll('.activiteit:checked');
-    
-    // Basis URL voor de kaart.
-    var newUrl = baseUrl;
+document.addEventListener('DOMContentLoaded', function () {
+    var checkboxes = document.querySelectorAll('.geonaam, .activiteit, .optie');
+    var iframe = document.getElementById('map');
+    var baseUrl = 'https://localfocuswidgets.net/65314588b3bb5?hide=dropdowns';
 
-    // Verzamelen van geselecteerde geonamen en activiteiten.
-    var geonamen = [];
-    geonaamCheckboxes.forEach(function (checkbox) {
-        geonamen.push(checkbox.getAttribute('data-geonaam'));
-    });
-
-    var activiteiten = [];
-    activiteitCheckboxes.forEach(function (checkbox) {
-        activiteiten.push(checkbox.getAttribute('data-activiteit'));
-    });
-
-    // Toevoegen van geonamen aan de URL.
-    geonamen.forEach(function (geonaam) {
-        newUrl += '&activate|geonaam=' + geonaam;
-    });
-
-    // Controleer of er activiteiten zijn. Zo niet, gebruik dan de geonamen als selectors.
-    if (activiteiten.length === 0) {
-        geonamen.forEach(function (geonaam) {
-            newUrl += '&activate|selector=' + geonaam;  // Gebruik geonaam als selector als er geen activiteiten zijn.
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener('change', function () {
+            updateMap();
         });
-    } else {
-        // Voor elke geonaam, maak voor elke activiteit een aparte selector.
+    });
+
+    function updateMap() {
+        var geonaamCheckboxes = document.querySelectorAll('.geonaam:checked');
+        var activiteitCheckboxes = document.querySelectorAll('.activiteit:checked');
+        var optieCheckboxes = document.querySelectorAll('.optie:checked');
+
+        // Basis URL voor de kaart.
+        var newUrl = baseUrl;
+
+        // Verzamelen van geselecteerde geonamen, activiteiten, en opties.
+        var geonamen = [];
+        geonaamCheckboxes.forEach(function (checkbox) {
+            geonamen.push(checkbox.getAttribute('data-geonaam'));
+        });
+
+        var activiteiten = [];
+        activiteitCheckboxes.forEach(function (checkbox) {
+            activiteiten.push(checkbox.getAttribute('data-activiteit'));
+        });
+
+        var opties = [];
+        optieCheckboxes.forEach(function (checkbox) {
+            opties.push(checkbox.getAttribute('data-optie'));
+        });
+
+        // Toevoegen van geonamen aan de URL.
         geonamen.forEach(function (geonaam) {
+            newUrl += '&activate|geonaam=' + geonaam;
+        });
+
+        // Logica voor het toevoegen van selectors op basis van geselecteerde geonamen, activiteiten, en opties.
+        geonamen.forEach(function (geonaam) {
+            if (activiteiten.length === 0 && opties.length === 0) {
+                // Als er geen activiteiten of opties zijn geselecteerd, wordt de geonaam de selector.
+                newUrl += '&activate|selector=' + geonaam;
+            } else {
+                // Voor elke combinatie van geonaam en activiteit, voeg een selector toe.
+                activiteiten.forEach(function (activiteit) {
+                    if (opties.length === 0) {
+                        // Als er geen opties zijn geselecteerd, combineer geonaam met activiteit.
+                        newUrl += '&activate|selector=' + geonaam + activiteit;
+                    } else {
+                        // Als er opties zijn geselecteerd, voeg deze toe aan de selector.
+                        opties.forEach(function (optie) {
+                            newUrl += '&activate|selector=' + geonaam + activiteit + optie;
+                        });
+                    }
+                });
+            }
+        });
+
+        // Als er geen geonamen zijn, voeg dan alleen activiteiten toe.
+        if (geonamen.length === 0) {
             activiteiten.forEach(function (activiteit) {
-                // Hier wordt voor elke activiteit een nieuwe selector-parameter gemaakt.
-                newUrl += '&activate|selector=' + geonaam + activiteit;
+                newUrl += '&activate|selector=' + activiteit;
             });
-        });
-    }
+        }
 
-    // Update de iframe URL.
-    iframe.src = newUrl;
-}
+        // Update de iframe URL.
+        iframe.src = newUrl;
+    }
+});
