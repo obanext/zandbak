90058d14de4ae275aaa684588fc9c3731ef459c9
obanext <93115438+obanext@users.noreply.github.com>
2023-10-23 13:39:50 +0200

    Update leefenleerkaart.js

--- leefenleerkaart.js
+++ leefenleerkaart.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var checkboxes = document.querySelectorAll('.geonaam-selector input, .activiteiten-soort input, .taal-opties input, .digitaal-opties input');
     var iframe = document.getElementById('map');
-    var baseUrl = 'https://localfocuswidgets.net/65314588b3bb5?hide=dropdowns';
+    var baseUrl = 'https://localfocuswidgets.net/6536375936ee8?hide=dropdowns';
 
     checkboxes.forEach(function (checkbox) {
         checkbox.addEventListener('change', function () {
