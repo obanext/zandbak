f5a6052a5bd47c3c894a6c0eab05df6c30417252
obanext <93115438+obanext@users.noreply.github.com>
2023-10-23 13:38:02 +0200

    Update leefenleerkaart.js

--- leefenleerkaart.js
+++ leefenleerkaart.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var checkboxes = document.querySelectorAll('.geonaam-selector input, .activiteiten-soort input, .taal-opties input, .digitaal-opties input');
-    var allAreasCheckbox = document.querySelector('#all-areas'); // Selecteer de "Alle gebieden" checkbox
     var iframe = document.getElementById('map');
-    var baseUrl = 'https://localfocuswidgets.net/6536375936ee8?hide=dropdowns';
+    var baseUrl = 'https://localfocuswidgets.net/65314588b3bb5?hide=dropdowns';
 
     checkboxes.forEach(function (checkbox) {
         checkbox.addEventListener('change', function () {
@@ -10,59 +9,58 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    allAreasCheckbox.addEventListener('change', function() {
-        var geonaamCheckboxes = document.querySelectorAll('.geonaam-selector input');
-        if (this.checked) {
-            geonaamCheckboxes.forEach(function(checkbox) {
-                checkbox.checked = false; // deselecteer andere geonamen als "Alle gebieden" is geselecteerd
-            });
-        }
-        updateMap(); // Update de kaart bij wijzigingen
-    });
-
     function updateMap() {
         var geonaamCheckboxes = document.querySelectorAll('.geonaam-selector input:checked');
         var activiteitCheckboxes = document.querySelectorAll('.activiteiten-soort input:checked');
         var taalOpties = document.querySelectorAll('.taal-opties input:checked');
         var digitaalOpties = document.querySelectorAll('.digitaal-opties input:checked');
-        var allAreasSelected = allAreasCheckbox.checked;
+
+        var geonamen = [];
+        geonaamCheckboxes.forEach(function (checkbox) {
+            geonamen.push(checkbox.value);
+        });
+
+        var activiteiten = [];
+        activiteitCheckboxes.forEach(function (checkbox) {
+            activiteiten.push(checkbox.value);
+        });
+
+        var taalOptieWaarden = [];
+        taalOpties.forEach(function (checkbox) {
+            taalOptieWaarden.push(checkbox.value);
+        });
+
+        var digitaalOptieWaarden = [];
+        digitaalOpties.forEach(function (checkbox) {
+            digitaalOptieWaarden.push(checkbox.value);
+        });
 
         var newUrl = baseUrl;
-        var allGeonamen = ['ac', 'an', 'az', 'azo', 'anw', 'aw', 'ao', 'awe', 'ad']; // Alle gebieden
 
-        if (allAreasSelected) {
-            allGeonamen.forEach(function(geonaam) {
-                newUrl += '&activate|geonaam=' + geonaam;
-                // De logica hier zou volgen wat er moet gebeuren wanneer "alle gebieden" is geselecteerd.
-                newUrl += '&activate|selector=' + geonaam; // Voorbeeldlogica
-            });
+        if (geonamen.length === 0 && activiteiten.length === 0) {
+            newUrl += '&activate|selector=aa'; // default selector wanneer niets is geselecteerd
         } else {
-            // Bestaande logica voor het afhandelen van individuele geonaam selecties
-            geonaamCheckboxes.forEach(function (checkbox) {
-                var geonaam = checkbox.value;
+            geonamen.forEach(function (geonaam) {
                 newUrl += '&activate|geonaam=' + geonaam;
-
-                if (activiteitCheckboxes.length === 0) {
-                    // Als er geen activiteiten zijn geselecteerd, voeg dan alleen de geonaam toe.
+                if (activiteiten.length === 0) {
                     newUrl += '&activate|selector=' + geonaam;
                 } else {
-                    activiteitCheckboxes.forEach(function (activiteitCheckbox) {
-                        var activiteit = activiteitCheckbox.value;
-                        newUrl += '&activate|selector=' + geonaam + activiteit; // combineer geonaam en activiteit
-                        
-                        var opties = (activiteit === 't' ? taalOpties : digitaalOpties);
-                        opties.forEach(function (optieCheckbox) {
-                            newUrl += '&activate|selector=' + geonaam + activiteit + optieCheckbox.value + '&'; // voeg opties toe
-                        });
+                    activiteiten.forEach(function (activiteit) {
+                        var opties = activiteit === 't' ? taalOptieWaarden : digitaalOptieWaarden;
+
+                        if (opties.length === 0) {
+                            newUrl += '&activate|selector=' + geonaam + activiteit;
+                        } else {
+                            opties.forEach(function (optie) {
+                                newUrl += '&activate|selector=' + geonaam + activiteit + optie + '&';
+                            });
+                        }
                     });
                 }
             });
         }
 
-        // Verwijder de laatste '&' als die er is
-        newUrl = newUrl.endsWith('&') ? newUrl.slice(0, -1) : newUrl;
-
-        // Stel de nieuwe URL in voor de iframe
-        iframe.src = newUrl;
+        newUrl = newUrl.endsWith('&') ? newUrl.slice(0, -1) : newUrl; // Verwijder het laatste '&' teken
+        iframe.src = newUrl; // Stel de nieuwe URL in voor de iframe
     }
 });
