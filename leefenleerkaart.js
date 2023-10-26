25bdb097f46d7386d2363b083a9d686163656b3e
obanext <93115438+obanext@users.noreply.github.com>
2023-10-26 05:53:30 +0200

    Update leefenleerkaart.js

--- leefenleerkaart.js
+++ leefenleerkaart.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var mapIframe = document.getElementById("map");
   var baseUrl = "https://localfocuswidgets.net/6538ef99859c1?hide=dropdowns";
+  var allAreasCheckbox = document.getElementById("all-areas"); // De checkbox voor 'alle gebieden'.
 
   // Functie om de geselecteerde geonamen op te halen
   function getSelectedGeonamen() {
@@ -33,17 +34,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var urlComponents = [];
 
     selectedGeonamen.forEach(function(geonaam) {
-      // Voeg de geonaam toe zonder activiteitstype
       urlComponents.push("&activate|geonaam=" + geonaam);
 
-      // Voeg de selector-component toe als er een activiteitstype is geselecteerd
       if (selectedActiviteiten.length > 0) {
         selectedActiviteiten.forEach(function(activiteit) {
           var selector = geonaam + activiteit;
           urlComponents.push("&activate|selector=" + selector);
         });
       } else {
-        // Voeg de selector-component toe voor elke geonaamselectie zonder activiteitstype
         urlComponents.push("&activate|selector=" + geonaam);
       }
     });
@@ -51,25 +49,55 @@ document.addEventListener("DOMContentLoaded", function() {
     return urlComponents.join("");
   }
 
-  // Functie om de kaart te updaten met de geselecteerde geonamen en activiteitstypen
-  function updateMapWithGeonamenAndActiviteiten() {
-    var geonamenSelectorsUrl = buildGeonamenSelectorsUrl();
-    var finalUrl = baseUrl + geonamenSelectorsUrl;
-    mapIframe.src = finalUrl;
+  // Functie om te controleren of 'alle gebieden' is geselecteerd
+  function isAllAreasSelected() {
+    return allAreasCheckbox.checked;
   }
 
-  // Voeg eventlisteners toe aan de geonaam- en activiteit-checkboxes om de kaart bij te werken wanneer er wijzigingen zijn
-  var geonamCheckboxes = document.querySelectorAll(".geonaam");
-  var activiteitCheckboxes = document.querySelectorAll(".activiteit");
-  
-  geonamCheckboxes.forEach(function(checkbox) {
-    checkbox.addEventListener("change", updateMapWithGeonamenAndActiviteiten);
-  });
+  // Functie om de URL-componenten voor alle geonamen samen te stellen
+  function buildAllGeonamenUrl() {
+    // Stel je voor dat dit alle mogelijke geonamen zijn, dit moet worden bijgewerkt.
+    var alleGeonamen = ['ac', 'an', 'az', 'azo', 'anw', 'aw', 'ao', 'awe', 'ad'];
+    var urlComponents = [];
 
-  activiteitCheckboxes.forEach(function(checkbox) {
-    checkbox.addEventListener("change", updateMapWithGeonamenAndActiviteiten);
-  });
+    alleGeonamen.forEach(function(geonaam) {
+      urlComponents.push("&activate|geonaam=" + geonaam);
+      urlComponents.push("&activate|selector=" + geonaam);
+    });
 
-  // Initialiseer de kaart met de standaard URL
-  mapIframe.src = baseUrl;
+    return urlComponents.join("");
+  }
+
+  // Functie om de kaart te updaten gebaseerd op de 'alle gebieden' selectie
+  function updateMapWithAllAreas() {
+    var allGeonamenUrl = buildAllGeonamenUrl();
+    mapIframe.src = baseUrl + allGeonamenUrl;
+  }
+
+  // Functie om de kaart te updaten met de geselecteerde geonamen en activiteitstypen
+  function updateMap() {
+    if (isAllAreasSelected()) {
+      updateMapWithAllAreas();
+    } else {
+      var urlComponents = buildGeonamenSelectorsUrl();
+      mapIframe.src = baseUrl + urlComponents;
+    }
+  }
+
+  // Event listeners voor checkboxes om de kaart bij te werken wanneer selecties veranderen
+  document.querySelectorAll('input[type=checkbox]').forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+      if (checkbox === allAreasCheckbox && checkbox.checked) {
+        // Deselecteer alle andere checkboxes als 'alle gebieden' is geselecteerd
+        document.querySelectorAll('input[type=checkbox]').forEach(function(box) {
+          if (box !== allAreasCheckbox) box.checked = false;
+        });
+      } else if (checkbox !== allAreasCheckbox && checkbox.checked) {
+        // Als een andere optie wordt geselecteerd, deselecteer 'alle gebieden'
+        allAreasCheckbox.checked = false;
+      }
+
+      updateMap(); // Update de kaart op basis van de huidige selecties
+    });
+  });
 });
