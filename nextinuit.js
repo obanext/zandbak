ddc42b826901c5ee4c10afe6cf0e45671aed471e
obanext <93115438+obanext@users.noreply.github.com>
2023-11-08 05:23:51 +0100

    Update nextinuit.js

--- nextinuit.js
+++ nextinuit.js
@@ -1,66 +1,110 @@
 // Een array om de objecten op te slaan
 let objectDatabase = [];
 
-// Deze functie wordt aangeroepen wanneer het document geladen is
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Event listener voor het object toevoegen formulier
-    document.getElementById('objectForm').addEventListener('submit', addObject);
+// Laad de objecten uit de lokale opslag wanneer de pagina laadt
+document.addEventListener('DOMContentLoaded', () => {
+    loadFromLocalStorage();
+
+    // Voeg een event listener toe aan het formulier als dat bestaat op deze pagina
+    const objectForm = document.getElementById('objectForm');
+    if (objectForm) {
+        objectForm.addEventListener('submit', addObject);
+    }
     
+    // Initialiseer de QR-scanner als de video element bestaat op deze pagina
+    const qrVideoElement = document.getElementById('qr-video');
+    if (qrVideoElement) {
+        initializeQRScanner();
+    }
+
     // Toon de initiele lijst van objecten
     displayObjects();
 });
 
-// Functie om een object toe te voegen
+function initializeQRScanner() {
+    let scanner = new Instascan.Scanner({ video: document.getElementById('qr-video') });
+
+    scanner.addListener('scan', function (content) {
+        console.log('QR Code gescand:', content);
+        updateObjectStatus(content);
+    });
+
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('Geen camera’s gevonden.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
+}
+
 function addObject(e) {
     e.preventDefault();
 
-    // Verzamel de waarden van het formulier
     let titel = document.getElementById('titel').value;
     let omschrijving = document.getElementById('omschrijving').value;
-    let datumIn = new Date().toISOString().split('T')[0]; // Vandaag's datum in ISO formaat
+    let datumIn = new Date().toISOString().split('T')[0];
 
-    // Maak een nieuw object en voeg het toe aan de database
     let newObject = {
-        id: objectDatabase.length + 1, // Simpele auto-increment ID
+        id: objectDatabase.length + 1,
         titel: titel,
         omschrijving: omschrijving,
         datumIn: datumIn,
-        datumUit: '', // Leeg voor nu
-        status: 'in' // Standaard status
+        datumUit: '',
+        status: 'in'
     };
 
     objectDatabase.push(newObject);
-    
-    // Reset het formulier
-    document.getElementById('objectForm').reset();
-
-    // Toon de bijgewerkte lijst
+    saveToLocalStorage();
     displayObjects();
+    
+    // Reset het formulier na het toevoegen van een object
+    document.getElementById('objectForm').reset();
 }
 
-// Functie om de lijst van objecten te tonen
+function updateObjectStatus(scannedId) {
+    let id = parseInt(scannedId);
+    let object = objectDatabase.find(obj => obj.id === id);
+
+    if (object) {
+        if (object.status === 'in') {
+            object.status = 'uit';
+            object.datumUit = new Date().toISOString().split('T')[0];
+        } else {
+            let newObject = {
+                ...object,
+                id: objectDatabase.length + 1,
+                datumIn: new Date().toISOString().split('T')[0],
+                datumUit: '',
+                status: 'in'
+            };
+            objectDatabase.push(newObject);
+        }
+        saveToLocalStorage();
+        displayObjects();
+    } else {
+        console.error('Object ID niet gevonden in de database:', id);
+    }
+}
+
 function displayObjects() {
-    let listContainer = document.getElementById('object-lijst');
-    listContainer.innerHTML = ''; // Leeg de container
-
-    // Maak een tabel en voeg de kolomkoppen toe
-    let table = document.createElement('table');
-    let headerRow = table.insertRow();
-    let headers = ["ID", "Titel", "Omschrijving", "Datum In", "Datum Uit", "Status"];
-    headers.forEach(headerText => {
-        let header = document.createElement('th');
-        header.textContent = headerText;
-        headerRow.appendChild(header);
-    });
-
-    // Voeg een rij toe voor elk object in de database
-    objectDatabase.forEach(obj => {
-        let row = table.insertRow();
-        Object.values(obj).forEach(text => {
-            let cell = row.insertCell();
-            cell.textContent = text;
-        });
-    });
-
-    listContainer.appendChild(table); // Voeg de tabel toe aan de container
+    // Voeg hier de code toe om objecten te tonen
+    // Dit kan variëren afhankelijk van hoe je de lijst in je HTML weergeeft
 }
+
+function saveToLocalStorage() {
+    localStorage.setItem('objectDatabase', JSON.stringify(objectDatabase));
+}
+
+function loadFromLocalStorage() {
+    let storedObjects = localStorage.getItem('objectDatabase');
+    if (storedObjects) {
+        objectDatabase = JSON.parse(storedObjects);
+        displayObjects();
+    }
+}
+
+// Voeg hier andere functies toe die je nodig hebt voor je app
+
