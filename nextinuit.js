ed1138f9576cc394cceaee162828bf784ab90373
obanext <93115438+obanext@users.noreply.github.com>
2023-11-08 04:38:22 +0100

    Update nextinuit.js

--- nextinuit.js
+++ nextinuit.js
@@ -1,21 +1,66 @@
+// Een array om de objecten op te slaan
+let objectDatabase = [];
+
+// Deze functie wordt aangeroepen wanneer het document geladen is
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Alle initialisatie code komt hier
+    // Event listener voor het object toevoegen formulier
+    document.getElementById('objectForm').addEventListener('submit', addObject);
     
-    // Functie om object toe te voegen
-    function addObject() {
-        // Implementatie komt hier
-    }
-    
-    // Functie om objectenlijst te weergeven
-    function displayObjects() {
-        // Implementatie komt hier
-    }
-    
-    // Functie voor het hanteren van de QR code scanner
-    function handleQRScanner() {
-        // Implementatie komt hier
-    }
-    
-    // Initialisatie functies
+    // Toon de initiele lijst van objecten
     displayObjects();
 });
+
+// Functie om een object toe te voegen
+function addObject(e) {
+    e.preventDefault();
+
+    // Verzamel de waarden van het formulier
+    let titel = document.getElementById('titel').value;
+    let omschrijving = document.getElementById('omschrijving').value;
+    let datumIn = new Date().toISOString().split('T')[0]; // Vandaag's datum in ISO formaat
+
+    // Maak een nieuw object en voeg het toe aan de database
+    let newObject = {
+        id: objectDatabase.length + 1, // Simpele auto-increment ID
+        titel: titel,
+        omschrijving: omschrijving,
+        datumIn: datumIn,
+        datumUit: '', // Leeg voor nu
+        status: 'in' // Standaard status
+    };
+
+    objectDatabase.push(newObject);
+    
+    // Reset het formulier
+    document.getElementById('objectForm').reset();
+
+    // Toon de bijgewerkte lijst
+    displayObjects();
+}
+
+// Functie om de lijst van objecten te tonen
+function displayObjects() {
+    let listContainer = document.getElementById('object-lijst');
+    listContainer.innerHTML = ''; // Leeg de container
+
+    // Maak een tabel en voeg de kolomkoppen toe
+    let table = document.createElement('table');
+    let headerRow = table.insertRow();
+    let headers = ["ID", "Titel", "Omschrijving", "Datum In", "Datum Uit", "Status"];
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        header.textContent = headerText;
+        headerRow.appendChild(header);
+    });
+
+    // Voeg een rij toe voor elk object in de database
+    objectDatabase.forEach(obj => {
+        let row = table.insertRow();
+        Object.values(obj).forEach(text => {
+            let cell = row.insertCell();
+            cell.textContent = text;
+        });
+    });
+
+    listContainer.appendChild(table); // Voeg de tabel toe aan de container
+}
