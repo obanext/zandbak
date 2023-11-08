da1f536b3be203f346ac1bccfc1a7a0f6507e02d
obanext <93115438+obanext@users.noreply.github.com>
2023-11-08 14:49:00 +0100

    Update nextinuit.js

--- nextinuit.js
+++ nextinuit.js
@@ -8,19 +8,32 @@ document.addEventListener('DOMContentLoaded', () => {
 function startQRScanner() {
     const html5QrCode = new Html5Qrcode("qr-reader");
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-
+    
     function onScanSuccess(decodedText, decodedResult) {
         console.log(`QR code gedetecteerd: ${decodedText}`);
-        // Doe iets met de gedecodeerde tekst, zoals het bijwerken van de status van een object.
-        // Je kunt ervoor kiezen om de scanner te stoppen of door te laten scannen.
-        // html5QrCode.stop(); // Uncomment deze lijn om de scanner te stoppen.
+        const scannedId = parseInt(decodedText);
+        // Aannemende dat objectDatabase beschikbaar is in deze scope:
+        const objectIndex = objectDatabase.findIndex(obj => obj.id === scannedId);
+        if (objectIndex !== -1) {
+            const object = objectDatabase[objectIndex];
+            if (object.status === 'in') {
+                object.status = 'uit';
+                object.datumUit = new Date().toISOString().split('T')[0];
+            } else if (object.status === 'uit') {
+                const newObject = {...object, id: objectDatabase.length + 1, datumIn: new Date().toISOString().split('T')[0], datumUit: '', status: 'in'};
+                objectDatabase.push(newObject);
+            }
+            saveToLocalStorage();
+            displayObjects();
+        } else {
+            console.error('Object ID niet gevonden in de database:', scannedId);
+        }
     }
-
+    
     function onScanFailure(error) {
-        // Log errors, maar spam niet de console als er gewoon geen QR code gevonden wordt.
         console.error(`QR scanfout: ${error}`);
     }
-
+    
     html5QrCode.start({ facingMode: "environment" }, config, onScanSuccess, onScanFailure)
         .catch((err) => {
             console.error(`Fout bij het starten van de QR scanner: ${err}`);
