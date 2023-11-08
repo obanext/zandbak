cd118973d44a88f8e5987114113d5f5cac6a996e
obanext <93115438+obanext@users.noreply.github.com>
2023-11-08 06:37:18 +0100

    Update nextinuit.js

--- nextinuit.js
+++ nextinuit.js
@@ -1,42 +1,41 @@
-// `nextinuit.js`
 document.addEventListener('DOMContentLoaded', () => {
-    if (document.getElementById('qr-video')) {
-        startVideoStream();
+    if (document.getElementById('qr-reader')) {
+        startQRScanner();
     }
 });
 
-function startVideoStream() {
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-        .then(function(stream) {
-            const video = document.getElementById('qr-video');
-            video.srcObject = stream;
-            video.setAttribute("playsinline", true); // vereist om te zeggen tegen iOS safari dat we geen fullscreen willen
-            video.play();
-            requestAnimationFrame(scanQRCode);
-        })
-        .catch(function(err) {
-            console.error("Error accessing the camera", err);
-        });
+function startQRScanner() {
+    const html5QrCode = new Html5Qrcode("qr-reader");
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+    // Deze functie wordt elke keer aangeroepen als er een QR-code wordt gescand.
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+        console.log(`QR Code detected: ${decodedText}`);
+        // Voer hier de logica uit die je wilt uitvoeren na het scannen van de QR-code.
+    };
+
+    // Als er een fout optreedt (bijv. camera toestemmingen zijn geweigerd)
+    const qrCodeErrorCallback = (errorMessage) => {
+        // Dit wordt aangeroepen bij fouten.
+        console.error(`QR Code no detected, error: ${errorMessage}`);
+    };
+
+    // Start met scannen van de camera.
+    Html5Qrcode.getCameras().then((cameras) => {
+        if (cameras && cameras.length) {
+            html5QrCode.start(
+                cameras[0].id, 
+                config, 
+                qrCodeSuccessCallback, 
+                qrCodeErrorCallback
+            ).catch((err) => {
+                console.error(`Unable to start QR scanner, error: ${err}`);
+            });
+        }
+    }).catch((err) => {
+        console.error(`Unable to get cameras, error: ${err}`);
+    });
 }
 
-function scanQRCode() {
-    const video = document.getElementById('qr-video');
-    if (video.readyState === video.HAVE_ENOUGH_DATA) {
-        const canvas = document.createElement('canvas');
-        const context = canvas.getContext('2d');
-        canvas.height = video.videoHeight;
-        canvas.width = video.videoWidth;
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        const qrCode = jsQR(imageData.data, imageData.width, imageData.height, {
-          inversionAttempts: "dontInvert",
-        });
-        
-        if (qrCode) {
-            console.log("Gevonden QR code", qrCode.data);
-            // Verwerk qrCode.data hier, bijvoorbeeld om de status van een object bij te werken
-        }
-    }
-    requestAnimationFrame(scanQRCode); // Blijf frames scannen
-}
+// Zorg ervoor dat je de juiste ID hebt voor je videocontainer.
+// In dit voorbeeld gebruiken we 'qr-reader' als ID voor de container waar de camera-invoer wordt getoond.
