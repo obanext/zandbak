dd13a41b619a0c04e7ee4e4d4de06c6104117abe
obanext <93115438+obanext@users.noreply.github.com>
2023-11-08 20:24:02 +0100

    Update mre.js

--- mre.js
+++ mre.js
@@ -1,32 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const video = document.getElementById('qr-video');
-    const canvasElement = document.getElementById('qr-canvas');
-    const canvas = canvasElement.getContext('2d');
+    const startButton = document.getElementById('start-button');
+    const stopButton = document.getElementById('stop-button');
+    const qrReaderElement = document.getElementById('qr-reader');
+    let html5QrCode;
 
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-    .then(function(stream) {
-        video.srcObject = stream;
-        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-        video.play();
-        requestAnimationFrame(tick);
+    startButton.addEventListener('click', () => {
+        startButton.style.display = 'none';
+        stopButton.style.display = 'block';
+        startQRScanner();
     });
 
-    function tick() {
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            canvasElement.height = video.videoHeight;
-            canvasElement.width = video.videoWidth;
-            canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-            var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-            var code = jsQR(imageData.data, imageData.width, imageData.height, {
-                inversionAttempts: "dontInvert",
+    stopButton.addEventListener('click', () => {
+        if (html5QrCode) {
+            html5QrCode.stop().then(() => {
+                console.log("QR scanning stopped.");
+                startButton.style.display = 'block';
+                stopButton.style.display = 'none';
+            }).catch((err) => {
+                console.error(`Failed to stop QR scanner: ${err}`);
             });
-            if (code) {
-                console.log("Found QR code", code.data);
-                // Stop scanning once the code is found.
-                video.srcObject.getTracks().forEach(track => track.stop());
-            } else {
-                requestAnimationFrame(tick);
-            }
         }
+    });
+
+    function startQRScanner() {
+        html5QrCode = new Html5Qrcode("qr-reader");
+        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+        
+        function onScanSuccess(decodedText, decodedResult) {
+            console.log(`QR code detected: ${decodedText}`);
+            html5QrCode.stop().then(() => {
+                console.log("QR scanning stopped after successful scan.");
+                stopButton.click();
+            }).catch((err) => {
+                console.error(`Failed to stop QR scanner: ${err}`);
+            });
+        }
+        
+        function onScanFailure(error) {
+            // Consider logging to the console for real-time feedback
+            console.error(`QR scan error: ${error}`);
+        }
+        
+        html5QrCode.start({ facingMode: "environment" }, config, onScanSuccess, onScanFailure)
+            .catch((err) => {
+                console.error(`Error in starting the QR scanner: ${err}`);
+            });
     }
 });
