d9e0eb65311ae3d1d17c9928faef47236d771041
obanext <93115438+obanext@users.noreply.github.com>
2023-11-08 06:07:07 +0100

    Update nextinuit.js

--- nextinuit.js
+++ nextinuit.js
@@ -1,32 +1,17 @@
-// Een array om de objecten op te slaan
-let objectDatabase = [];
-
-// Laad de objecten uit de lokale opslag wanneer de pagina laadt
+// `nextinuit.js`
 document.addEventListener('DOMContentLoaded', () => {
-    loadFromLocalStorage();
-
-    // Voeg een event listener toe aan het formulier als dat bestaat op deze pagina
-    const objectForm = document.getElementById('objectForm');
-    if (objectForm) {
-        objectForm.addEventListener('submit', addObject);
-    }
-    
-    // Initialiseer de QR-scanner als de video element bestaat op deze pagina
-    const qrVideoElement = document.getElementById('qr-video');
-    if (qrVideoElement) {
+    // De QR-scanner moet alleen initialiseren op de pagina met de qr-video element
+    if (document.getElementById('qr-video')) {
         initializeQRScanner();
     }
-
-    // Toon de initiele lijst van objecten
-    displayObjects();
 });
 
 function initializeQRScanner() {
     let scanner = new Instascan.Scanner({ video: document.getElementById('qr-video') });
-
     scanner.addListener('scan', function (content) {
         console.log('QR Code gescand:', content);
-        updateObjectStatus(content);
+        // Hier zou je de code plaatsen om de status van het object bij te werken
+        // Dit vereist dat je ofwel toegang hebt tot de objectDatabase of een andere manier om de data op te halen
     });
 
     Instascan.Camera.getCameras().then(function (cameras) {
@@ -39,72 +24,3 @@ function initializeQRScanner() {
         console.error(e);
     });
 }
-
-function addObject(e) {
-    e.preventDefault();
-
-    let titel = document.getElementById('titel').value;
-    let omschrijving = document.getElementById('omschrijving').value;
-    let datumIn = new Date().toISOString().split('T')[0];
-
-    let newObject = {
-        id: objectDatabase.length + 1,
-        titel: titel,
-        omschrijving: omschrijving,
-        datumIn: datumIn,
-        datumUit: '',
-        status: 'in'
-    };
-
-    objectDatabase.push(newObject);
-    saveToLocalStorage();
-    displayObjects();
-    
-    // Reset het formulier na het toevoegen van een object
-    document.getElementById('objectForm').reset();
-}
-
-function updateObjectStatus(scannedId) {
-    let id = parseInt(scannedId);
-    let object = objectDatabase.find(obj => obj.id === id);
-
-    if (object) {
-        if (object.status === 'in') {
-            object.status = 'uit';
-            object.datumUit = new Date().toISOString().split('T')[0];
-        } else {
-            let newObject = {
-                ...object,
-                id: objectDatabase.length + 1,
-                datumIn: new Date().toISOString().split('T')[0],
-                datumUit: '',
-                status: 'in'
-            };
-            objectDatabase.push(newObject);
-        }
-        saveToLocalStorage();
-        displayObjects();
-    } else {
-        console.error('Object ID niet gevonden in de database:', id);
-    }
-}
-
-function displayObjects() {
-    // Voeg hier de code toe om objecten te tonen
-    // Dit kan variëren afhankelijk van hoe je de lijst in je HTML weergeeft
-}
-
-function saveToLocalStorage() {
-    localStorage.setItem('objectDatabase', JSON.stringify(objectDatabase));
-}
-
-function loadFromLocalStorage() {
-    let storedObjects = localStorage.getItem('objectDatabase');
-    if (storedObjects) {
-        objectDatabase = JSON.parse(storedObjects);
-        displayObjects();
-    }
-}
-
-// Voeg hier andere functies toe die je nodig hebt voor je app
-
