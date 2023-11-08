0738e9574682a5457fb5eb5010ed38ef1405e773
obanext <93115438+obanext@users.noreply.github.com>
2023-11-08 20:10:52 +0100

    Update mre.js

--- mre.js
+++ mre.js
@@ -1,24 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const qrReaderElement = document.getElementById('qr-reader');
-    let html5QrCode;
+    const video = document.getElementById('qr-video');
+    const canvasElement = document.getElementById('qr-canvas');
+    const canvas = canvasElement.getContext('2d');
 
-    function onScanSuccess(decodedText, decodedResult) {
-        console.log(`QR code detected: ${decodedText}`);
-        html5QrCode.stop();
-    }
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+    .then(function(stream) {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+        video.play();
+        requestAnimationFrame(tick);
+    });
 
-    function onScanFailure(error) {
-        console.error(`QR scan error: ${error}`);
-    }
-
-    function startQRScanner() {
-        html5QrCode = new Html5Qrcode("qr-reader");
-        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-        html5QrCode.start({ facingMode: "environment" }, config, onScanSuccess, onScanFailure)
-            .catch((err) => {
-                console.error(`Error in starting the QR scanner: ${err}`);
+    function tick() {
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+            canvasElement.height = video.videoHeight;
+            canvasElement.width = video.videoWidth;
+            canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+            var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+            var code = jsQR(imageData.data, imageData.width, imageData.height, {
+                inversionAttempts: "dontInvert",
             });
+            if (code) {
+                console.log("Found QR code", code.data);
+                // Stop scanning once the code is found.
+                video.srcObject.getTracks().forEach(track => track.stop());
+            } else {
+                requestAnimationFrame(tick);
+            }
+        }
     }
-
-    startQRScanner(); // Start scanner on load for MRE
 });
