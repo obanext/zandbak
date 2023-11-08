70cfa34b5e5ba9b30be570ad07a0cab709f4a4b4
obanext <93115438+obanext@users.noreply.github.com>
2023-11-08 04:31:55 +0100

    Create nextinuit.js

---
+++ nextinuit.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Alle initialisatie code komt hier
+    
+    // Functie om object toe te voegen
+    function addObject() {
+        // Implementatie komt hier
+    }
+    
+    // Functie om objectenlijst te weergeven
+    function displayObjects() {
+        // Implementatie komt hier
+    }
+    
+    // Functie voor het hanteren van de QR code scanner
+    function handleQRScanner() {
+        // Implementatie komt hier
+    }
+    
+    // Initialisatie functies
+    displayObjects();
+});
