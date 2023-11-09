07397a6baa4ed5cb4d3fd42b78f5fd8c1226f6ac
obanext <93115438+obanext@users.noreply.github.com>
2023-11-09 11:41:49 +0100

    Update nextinuitadmin.js

--- nextinuitadmin.js
+++ nextinuitadmin.js
@@ -1,21 +1,18 @@
-// nextinuitadmin.js
-// The new_id_base constant should be defined within this script.
 const new_id_base = 'nextinuit00000';
 
 document.addEventListener('DOMContentLoaded', () => {
-    displayObjects(); // We're assuming displayObjects will also handle loading from local storage.
+    displayObjects();
     document.getElementById('objectForm').addEventListener('submit', addObject);
+    document.getElementById('csvFileInput').addEventListener('change', importCsv); // Gewijzigd
 });
 
 function addObject(e) {
     e.preventDefault();
     const titel = document.getElementById('titel').value;
-    const omschrijving = document.getElementById('omschrijving').value;
     const datumIn = new Date().toISOString().split('T')[0];
     const newObject = {
         id: `${new_id_base}${String(objectDatabase.length + 1).padStart(5, '0')}`,
         titel: titel,
-        omschrijving: omschrijving,
         datumIn: datumIn,
         datumUit: '',
         status: 'in'
@@ -32,12 +29,43 @@ function displayObjects() {
     objectDatabase.forEach(obj => {
         const itemDiv = document.createElement('div');
         itemDiv.className = 'object-item';
-        itemDiv.textContent = `ID: ${obj.id} Titel: ${obj.titel} Omschrijving: ${obj.omschrijving} Datum In: ${obj.datumIn} Datum Uit: ${obj.datumUit} Status: ${obj.status}`;
+        itemDiv.textContent = `ID: ${obj.id} Titel: ${obj.titel} Datum In: ${obj.datumIn} Datum Uit: ${obj.datumUit} Status: ${obj.status}`;
         listContainer.appendChild(itemDiv);
     });
 }
 
-// The downloadObjectAsCsv and convertToCSV functions remain unchanged.
+function importCsv() {
+    const fileInput = document.getElementById('csvFileInput');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function(event) {
+            const csvData = event.target.result;
+            objectDatabase = parseCsv(csvData); // Overschrijft de huidige database
+            saveToLocalStorage(); // Opslaan in lokale opslag
+            displayObjects(); // Update de weergave
+        };
+    } else {
+        alert('Geen bestand geselecteerd');
+    }
+}
+
+function parseCsv(data) {
+    const csvRows = data.split(/\r\n|\n/); // Splits de rijen
+    const headers = csvRows[0].split(','); // Haal de headers op
+
+    return csvRows.slice(1).map(row => {
+        const values = row.split(',');
+        const obj = headers.reduce((object, header, index) => {
+            object[header] = values[index];
+            return object;
+        }, {});
+        return obj;
+    });
+}
+
 function downloadObjectAsCsv(exportObj, exportName) {
     const csvStr = convertToCSV(exportObj);
     const dataUri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvStr);
@@ -51,9 +79,14 @@ function downloadObjectAsCsv(exportObj, exportName) {
 
 function convertToCSV(objArray) {
     const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-    let csvStr = `${Object.keys(array[0]).join(',')}\r\n`;
+    let csvStr = '';
+    // Verwijder 'omschrijving' van de headers
+    const headers = Object.keys(array[0]).filter(header => header !== 'omschrijving');
+    csvStr += headers.join(',') + '\r\n';
     for (const row of array) {
-        csvStr += `${Object.values(row).join(',')}\r\n`;
+        // Verwijder 'omschrijving' van elke rij
+        const filteredRow = headers.map(header => row[header]);
+        csvStr += filteredRow.join(',') + '\r\n';
     }
     return csvStr;
 }
