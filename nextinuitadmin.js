bdebc85cc68a09a2e2e3229bcd4057ce2d688dbf
obanext <93115438+obanext@users.noreply.github.com>
2023-11-13 14:18:38 +0100

    Update nextinuitadmin.js

--- nextinuitadmin.js
+++ nextinuitadmin.js
@@ -98,80 +98,90 @@ function convertToCSV(objArray) {
 }
 
 function importCSVAndGenerateQR() {
-  const fileInput = document.getElementById('csvFileInput');
-  if (!fileInput) {
-    console.error('CSV file input element not found');
-    return;
-  }
-
-  fileInput.click(); // Open the file dialog
-
-  fileInput.onchange = e => {
-    let busyIndicator = document.getElementById('busyIndicator');
-    if (!busyIndicator) {
-      busyIndicator = document.createElement('div');
-      busyIndicator.id = 'busyIndicator';
-      busyIndicator.innerText = 'Bezig...'; 
-      document.body.appendChild(busyIndicator);
+    const fileInput = document.getElementById('csvFileInput');
+    if (!fileInput) {
+        console.error('CSV file input element not found');
+        return;
     }
-    busyIndicator.style.display = 'block';
 
-    let file = e.target.files[0];
-    Papa.parse(file, {
-      complete: function(results) {
-        let processedCount = 0; 
+    fileInput.click(); // Open the file dialog
 
-        if (Array.isArray(results.data)) {
-          results.data.forEach((row, index) => {
-            if (Array.isArray(row) && row.length > 0 && row[0].trim() !== '') {
-              try {
-                let qrCode = new QRCode(document.createElement('div'), {
-                  text: row[0],
-                  width: 128,
-                  height: 128,
-                  correctLevel: QRCode.CorrectLevel.H
-                });
-
-                let canvas = qrCode._oDrawing._elCanvas;
-                let filename = row[0].replace(/\W+/g, '_') + '.png';
-
-                canvas.toBlob(function(blob) {
-                  let link = document.createElement('a');
-                  link.download = filename;
-                  link.href = URL.createObjectURL(blob);
-                  link.click();
-
-                  processedCount++;
-                  if (processedCount === results.data.length) {
-                    busyIndicator.style.display = 'none';
-                  }
-                });
-              } catch (error) {
-                console.error('Error generating QR code for row: ', row, error);
-              }
-            } else {
-              processedCount++; // Increment even if the row is not processed
-              if (processedCount === results.data.length) {
-                busyIndicator.style.display = 'none';
-              }
-            }
-          });
-        } else {
-          console.error('CSV parse did not return a valid array.');
-          busyIndicator.style.display = 'none';
+    fileInput.onchange = e => {
+        let busyIndicator = document.getElementById('busyIndicator');
+        if (!busyIndicator) {
+            busyIndicator = document.createElement('div');
+            busyIndicator.id = 'busyIndicator';
+            busyIndicator.innerText = 'Bezig...'; // Dutch for 'Busy...'
+            document.body.appendChild(busyIndicator);
         }
-      },
-      error: function(err) {
-        console.error('Error parsing CSV: ', err);
-        busyIndicator.style.display = 'none';
-      }
-    });
-  };
+        busyIndicator.style.display = 'block';
+
+        let file = e.target.files[0];
+        Papa.parse(file, {
+            complete: function(results) {
+                let processedCount = 0; // To keep track of how many QR codes have been processed
+
+                if (Array.isArray(results.data)) {
+                    results.data.forEach((row, index) => {
+                        if (Array.isArray(row) && row.length > 0 && row[0].trim() !== '') {
+                            try {
+                                // Generate QR Code for each row using qrcode-generator
+                                let qr = qrcode(0, 'L');
+                                qr.addData(row[0]);
+                                qr.make();
+                                let imgTag = qr.createImgTag(4); // Creates an <img> tag as a string
+
+                                // Convert image tag to a downloadable image
+                                let tempDiv = document.createElement('div');
+                                tempDiv.innerHTML = imgTag;
+                                let imgElement = tempDiv.firstChild;
+                                let canvas = document.createElement('canvas');
+                                let context = canvas.getContext('2d');
+                                let image = new Image();
+                                image.onload = function() {
+                                    canvas.width = image.width;
+                                    canvas.height = image.height;
+                                    context.drawImage(image, 0, 0);
+                                    canvas.toBlob(function(blob) {
+                                        let filename = row[0].replace(/\W+/g, '_') + '.png';
+                                        let link = document.createElement('a');
+                                        link.download = filename;
+                                        link.href = URL.createObjectURL(blob);
+                                        link.click();
+                                        processedCount++;
+                                        if (processedCount === results.data.length) {
+                                            busyIndicator.style.display = 'none';
+                                        }
+                                    });
+                                };
+                                image.src = imgElement.src;
+                            } catch (error) {
+                                console.error('Error generating QR code for row: ', row, error);
+                                processedCount++;
+                                if (processedCount === results.data.length) {
+                                    busyIndicator.style.display = 'none';
+                                }
+                            }
+                        } else {
+                            processedCount++; // Increment even if the row is not processed
+                            if (processedCount === results.data.length) {
+                                busyIndicator.style.display = 'none';
+                            }
+                        }
+                    });
+                } else {
+                    console.error('CSV parse did not return a valid array.');
+                    busyIndicator.style.display = 'none';
+                }
+            },
+            error: function(err) {
+                console.error('Error parsing CSV: ', err);
+                busyIndicator.style.display = 'none';
+            }
+        });
+    };
 }
 
-function saveToLocalStorage() {
-    localStorage.setItem('objectDatabase', JSON.stringify(objectDatabase));
-}
 
 // Nieuwe filterfuncties
 function filterById(id) {
