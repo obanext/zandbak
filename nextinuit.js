6c4e792e7609e5ee3f6b38cd1d50f5fbfa3bc14a
obanext <93115438+obanext@users.noreply.github.com>
2023-11-15 11:36:00 +0100

    Update nextinuit.js

--- nextinuit.js
+++ nextinuit.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('start-button');
     const stopButton = document.getElementById('stop-button');
     const qrReaderElement = document.getElementById('qr-reader');
+    const resultElement = document.getElementById('scan-result'); // Veronderstelt dat dit element bestaat in uw HTML.
     let html5QrCode;
 
     // Placeholder image element
@@ -10,103 +11,102 @@ document.addEventListener('DOMContentLoaded', function () {
     placeholderImage.src = 'nextxbb.png';
     placeholderImage.alt = 'Start Scanner';
     placeholderImage.style.cursor = 'pointer';
-    placeholderImage.style.display = 'block'; 
+    placeholderImage.style.display = 'block';
     qrReaderElement.parentNode.insertBefore(placeholderImage, qrReaderElement);
 
     startButton.addEventListener('click', function () {
         startButton.style.display = 'none';
         stopButton.style.display = 'block';
         qrReaderElement.style.display = 'block';
-        placeholderImage.style.display = 'none'; 
+        placeholderImage.style.display = 'none';
         if (qrReaderElement) {
             startQRScanner();
         }
     });
 
     stopButton.addEventListener('click', function () {
-        stopButton.style.display = 'none';
-        startButton.style.display = 'block';
-        qrReaderElement.style.display = 'none';
-        placeholderImage.style.display = 'block'; 
-        if (html5QrCode) {
-            html5QrCode.stop().then(() => {
-                console.log("QR scanning stopped.");
-            }).catch((err) => {
-                console.error(`Failed to stop QR scanner: ${err}`);
-            });
-        }
-    });
+    stopButton.style.display = 'none';
+    startButton.style.display = 'block';
+    qrReaderElement.style.display = 'none';
+    placeholderImage.style.display = 'block';
+    if (html5QrCode) {
+        html5QrCode.stop().then(() => {
+            console.log("QR scanning stopped.");
+        }).catch((err) => {
+            console.error(`Failed to stop QR scanner: ${err}`);
+        });
+    }
+});
 
     placeholderImage.addEventListener('click', function () {
-        startButton.click(); 
+        startButton.click();
+    });
 
- function startQRScanner() {
+   // ... [de rest van je code voor deze sectie]
+
+function startQRScanner() {
     html5QrCode = new Html5Qrcode("qr-reader");
     const readerWidth = qrReaderElement.offsetWidth;
     const qrboxSize = Math.min(300, readerWidth);
-    // Ensure the qrboxSize is at least 50px
-    const correctedQrboxSize = Math.max(qrboxSize, 90);
-    const config = {
+    const correctedQrboxSize = Math.max(qrboxSize, 50);
+    html5QrCode.start({ facingMode: "environment" }, {
         fps: 10,
-        qrbox: { width: correctedQrboxSize, height: correctedQrboxSize },
-        facingMode: "environment" // This is now included in the config object
-    };
-
-    // Now pass the correct config object to the start method
-    html5QrCode.start(config, onScanSuccess, onScanFailure)
-        .catch((err) => {
-            console.error(`Error in starting the QR scanner: ${err}`);
-        });
-}
-}
-   function onScanSuccess(decodedText, decodedResult) {
-    console.log(`QR code detected: ${decodedText}`);
-    html5QrCode.stop().then(() => {
-        console.log("QR scanning stopped after successful scan.");
-        stopButton.click(); 
-        const resultElement = document.getElementById('scan-result');
-
-       
-        const objectIndices = objectDatabase
-            .map((obj, index) => ({ id: obj.id, index }))
-            .filter(obj => obj.id === decodedText)
-            .map(obj => obj.index);
-
-        const lastObjectIndex = objectIndices.length > 0 ? objectIndices[objectIndices.length - 1] : -1;
-
-        if (lastObjectIndex !== -1) {
-            const object = objectDatabase[lastObjectIndex];
-            if (object.status === 'in') {
-                object.status = 'uit';
-                object.datumUit = new Date().toISOString().split('T')[0];
-                resultElement.textContent = `je hebt: "${object.titel}" geleend`;
-            } else {
-                const newObject = {
-                    id: object.id,
-                    titel: object.titel,
-                    omschrijving: object.omschrijving,
-                    datumIn: new Date().toISOString().split('T')[0],
-                    datumUit: '',
-                    status: 'in'
-                };
-                objectDatabase.push(newObject);
-                resultElement.textContent = `Dank voor het terugbrengen van: "${object.titel}"`;
-            }
-            saveToLocalStorage();
-        } else {
-            resultElement.textContent = "We weten niet wat dit is. Scan nog een keer of vraag het even";
-        }
-    }).catch((err) => {
-        console.error(`Failed to stop QR scanner: ${err}`);
+        qrbox: { width: correctedQrboxSize, height: correctedQrboxSize }
+    }, onScanSuccess, onScanFailure)
+    .catch((err) => {
+        console.error(`Error in starting the QR scanner: ${err}`);
     });
 }
 
+
+
+    // De eerder verstrekte onScanSuccess en onScanFailure functies zijn hier geïntegreerd.
+    function onScanSuccess(decodedText, decodedResult) {
+        console.log(`QR code detected: ${decodedText}`);
+        html5QrCode.stop().then(() => {
+            console.log("QR scanning stopped after successful scan.");
+            stopButton.click();
+
+            // Aangenomen dat objectDatabase en saveToLocalStorage beschikbaar zijn in de scope.
+            const objectIndices = objectDatabase
+                .map((obj, index) => ({ id: obj.id, index }))
+                .filter(obj => obj.id === decodedText)
+                .map(obj => obj.index);
+
+            const lastObjectIndex = objectIndices.length > 0 ? objectIndices[objectIndices.length - 1] : -1;
+
+            if (lastObjectIndex !== -1) {
+                const object = objectDatabase[lastObjectIndex];
+                if (object.status === 'in') {
+                    object.status = 'uit';
+                    object.datumUit = new Date().toISOString().split('T')[0];
+                    resultElement.textContent = `je hebt: "${object.titel}" geleend`;
+                } else {
+                    const newObject = {
+                        id: object.id,
+                        titel: object.titel,
+                        omschrijving: object.omschrijving,
+                        datumIn: new Date().toISOString().split('T')[0],
+                        datumUit: '',
+                        status: 'in'
+                    };
+                    objectDatabase.push(newObject);
+                    resultElement.textContent = `Dank voor het terugbrengen van: "${object.titel}"`;
+                }
+                saveToLocalStorage();
+            } else {
+                resultElement.textContent = "We weten niet wat dit is. Scan nog een keer of vraag het even";
+            }
+        }).catch((err) => {
+            console.error(`Failed to stop QR scanner: ${err}`);
+        });
+    }
 
     function onScanFailure(error) {
         console.error(`QR scan error: ${error}`);
     }
 
-   
-    qrReaderElement.style.display = 'none'; 
-    placeholderImage.style.display = 'block'; 
-}});
+    // Zorg dat deze elementen opnieuw worden getoond als er geen scan plaatsvindt.
+    qrReaderElement.style.display = 'none';
+    placeholderImage.style.display = 'block';
+});
