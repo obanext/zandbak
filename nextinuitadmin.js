2e98b6d009bb45d81ae14b9bfa04c15709ebd17e
obanext <93115438+obanext@users.noreply.github.com>
2023-11-15 06:38:21 +0100

    Update nextinuitadmin.js

--- nextinuitadmin.js
+++ nextinuitadmin.js
@@ -1,208 +1,161 @@
-const new_id_base = 'nextinuit00000';
+	const new_id_base = 'nextinuit00000';
+	let lastChecked = null; 
 
-document.addEventListener('DOMContentLoaded', () => {
-    displayObjects();
-    document.getElementById('objectForm').addEventListener('submit', addObject);
-    document.getElementById('csvFileInput').addEventListener('change', importCsv);
-    document.getElementById('search-button').addEventListener('click', performSearch);
-    document.getElementById('clear-button').addEventListener('click', clearSearch);
-});
+	document.addEventListener('DOMContentLoaded', () => {
+		displayObjects();
+		document.getElementById('objectForm').addEventListener('submit', addObject);
+		document.getElementById('csvFileInput').addEventListener('change', importCsv);
+		document.getElementById('search-button').addEventListener('click', performSearch);
+		document.getElementById('clear-button').addEventListener('click', clearSearch);
+		document.getElementById('generate-qr-button').addEventListener('click', generateQRForSearchResults);
+		document.getElementById('export-csv-button').addEventListener('click', () => {
+			downloadObjectAsCsv(objectDatabase);
+		});
+		loadFromLocalStorage();
 
-function addObject(e) {
-    e.preventDefault();
-    const titel = document.getElementById('titel').value;
-    const datumIn = new Date().toISOString().split('T')[0];
-    const newObject = {
-        id: `${new_id_base}${String(objectDatabase.length + 1).padStart(5, '0')}`,
-        titel: titel,
-        datumIn: datumIn,
-        datumUit: '',
-        status: 'in'
-    };
-    objectDatabase.push(newObject);
-    saveToLocalStorage();
-    displayObjects();
-    e.target.reset();
-}
+		
+		const statusIn = document.getElementById('status-in');
+		const statusUit = document.getElementById('status-uit');
+		[statusIn, statusUit].forEach(radio => {
+			radio.addEventListener('click', function() {
+				if (this === lastChecked) {
+					this.checked = false;
+					lastChecked = null;
+				} else {
+					lastChecked = this;
+				}
+			});
+		});
+	});
 
-function displayObjects(objects = objectDatabase) {
-    const listContainer = document.getElementById('object-lijst');
-    listContainer.innerHTML = '';
-    objects.forEach(obj => {
-        const itemDiv = document.createElement('div');
-        itemDiv.className = 'object-item';
-        itemDiv.textContent = `ID: ${obj.id} Titel: ${obj.titel} Datum In: ${obj.datumIn} Datum Uit: ${obj.datumUit} Status: ${obj.status}`;
-        listContainer.appendChild(itemDiv);
-    });
-}
 
-function importCsv(e) {
-    const fileInput = document.getElementById('csvFileInput');
-    const file = fileInput.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = function(event) {
-            const csvData = event.target.result;
-            objectDatabase = parseCsv(csvData);
-            saveToLocalStorage();
-            displayObjects();
-        };
-    } else {
-        alert('Geen bestand geselecteerd');
-    }
-}
+	function addObject(e) {
+		e.preventDefault();
+		const titel = document.getElementById('titel').value;
+		const new_id = `${new_id_base}${String(objectDatabase.length + 1).padStart(5, '0')}`;
+		const datumIn = new Date().toISOString().split('T')[0];
+		const newObject = {
+			id: new_id,
+			titel: titel,
+			datumIn: datumIn,
+			datumUit: '',
+			status: 'in'
+		};
+		objectDatabase.push(newObject);
+		saveToLocalStorage();
+		displayObjects();
+		e.target.reset();
+		downloadQRCode(new_id, `QR_${new_id}.png`);
+	}
 
-function parseCsv(data) {
-    const csvRows = data.split(/\r\n|\n/);
-    const headers = csvRows[0].split(',');
-    return csvRows.slice(1).map(row => {
-        const values = row.split(',');
-        const obj = headers.reduce((object, header, index) => {
-            object[header] = values[index];
-            return object;
-        }, {});
-        return obj;
-    });
-}
+	function displayObjects(objects = objectDatabase) {
+		const listContainer = document.querySelector('.object-list-container');
+		listContainer.innerHTML = '';
+		objects.forEach(obj => {
+			const itemDiv = document.createElement('div');
+			itemDiv.className = 'object-item';
+			itemDiv.style.fontFamily = "'AvenirLight', 'Arial', sans-serif";
+			itemDiv.textContent = `ID: ${obj.id} Titel: ${obj.titel} Datum In: ${obj.datumIn} Datum Uit: ${obj.datumUit} Status: ${obj.status}`;
+			listContainer.appendChild(itemDiv);
+		});
+	}
 
-function importCSVAndGenerateQR() {
-    const fileInput = document.getElementById('csvFileInput');
-    if (!fileInput) {
-        console.error('CSV file input element not found');
-        return;
-    }
 
-    fileInput.click(); // Open the file dialog
+	function performSearch() {
+		const searchId = document.getElementById('search-id').value.trim();
+		const searchTitle = document.getElementById('search-title').value.trim().toLowerCase();
+		const statusIn = document.getElementById('status-in').checked;
+		const statusUit = document.getElementById('status-uit').checked;
 
-    fileInput.onchange = e => {
-        let busyIndicator = document.getElementById('busyIndicator');
-        if (!busyIndicator) {
-            busyIndicator = document.createElement('div');
-            busyIndicator.id = 'busyIndicator';
-            busyIndicator.innerText = 'Bezig...'; // Dutch for 'Busy...'
-            document.body.appendChild(busyIndicator);
-        }
-        busyIndicator.style.display = 'block';
-
-        let file = e.target.files[0];
-        Papa.parse(file, {
-            complete: function(results) {
-                let processedCount = 0; // To keep track of how many QR codes have been processed
-                let temporaryDatabase = []; // Temporary database for CSV data
-
-                if (Array.isArray(results.data)) {
-                    results.data.forEach((row, index) => {
-                        if (Array.isArray(row) && row.length > 0 && row[0].trim() !== '') {
-                            try {
-                                // Generate QR Code for each row using qrcode-generator
-                                let qr = qrcode(0, 'L');
-                                qr.addData(row[0]);
-                                qr.make();
-                                let imgTag = qr.createImgTag(4); // Creates an <img> tag as a string
-
-                                // Convert image tag to a downloadable image
-                                let tempDiv = document.createElement('div');
-                                tempDiv.innerHTML = imgTag;
-                                let imgElement = tempDiv.firstChild;
-                                let canvas = document.createElement('canvas');
-                                let context = canvas.getContext('2d');
-                                let image = new Image();
-                                image.onload = function() {
-                                    canvas.width = image.width;
-                                    canvas.height = image.height;
-                                    context.drawImage(image, 0, 0);
-                                    canvas.toBlob(function(blob) {
-                                        let filename = row[0].replace(/\W+/g, '_') + '.png';
-                                        let link = document.createElement('a');
-                                        link.download = filename;
-                                        link.href = URL.createObjectURL(blob);
-                                        link.click();
-                                        processedCount++;
-                                        if (processedCount === results.data.length) {
-                                            busyIndicator.style.display = 'none';
-                                        }
-                                    });
-                                };
-                                image.src = imgElement.src;
-
-                                // Add row data to the temporary database
-                                temporaryDatabase.push(row);
-                            } catch (error) {
-                                console.error('Error generating QR code for row: ', row, error);
-                                processedCount++;
-                                if (processedCount === results.data.length) {
-                                    busyIndicator.style.display = 'none';
-                                }
-                            }
-                        } else {
-                            processedCount++; // Increment even if the row is not processed
-                            if (processedCount === results.data.length) {
-                                busyIndicator.style.display = 'none';
-                            }
-                        }
-                    });
-                    
-                    // Here you can use the temporaryDatabase for further processing if needed
-                } else {
-                    console.error('CSV parse did not return a valid array.');
-                    busyIndicator.style.display = 'none';
-                }
-            },
-            error: function(err) {
-                console.error('Error parsing CSV: ', err);
-                busyIndicator.style.display = 'none';
-            }
-        });
-    };
-}
+		let results = [...objectDatabase];
+		if (searchId) {
+			results = results.filter(obj => obj.id.includes(searchId));
+		}
+		if (searchTitle) {
+		   
+			results = results.filter(obj => obj.titel && obj.titel.toLowerCase().includes(searchTitle));
+		}
+		if (statusIn) {
+			results = results.filter(obj => obj.status === 'in');
+		} else if (statusUit) {
+			results = results.filter(obj => obj.status === 'uit');
+		}
+		
+		displayObjects(results);
+		return results;
+	}
 
 
 
-// Nieuwe filterfuncties
-function filterById(id) {
-    return objectDatabase.filter(obj => obj.id.toLowerCase().includes(id.toLowerCase()));
-}
 
-function filterByTitle(title) {
-    return objectDatabase.filter(obj => obj.titel.toLowerCase().includes(title.toLowerCase()));
-}
+	function clearSearch() {
+		document.getElementById('search-id').value = '';
+		document.getElementById('search-title').value = '';
+		document.getElementById('status-in').checked = false;
+		document.getElementById('status-uit').checked = false;
+		displayObjects();
+	}
 
-function filterByStatus(status) {
-    return objectDatabase.filter(obj => obj.status === status);
-}
+	function saveToLocalStorage() {
+		localStorage.setItem('objectDatabase', JSON.stringify(objectDatabase));
+	}
 
-// Functie die wordt aangeroepen bij het klikken op de zoekknop
-function performSearch() {
-    const searchId = document.getElementById('search-id').value;
-    const searchTitle = document.getElementById('search-title').value;
-    const statusIn = document.getElementById('status-in').checked;
-    const statusUit = document.getElementById('status-uit').checked;
+	function loadFromLocalStorage() {
+		const storedObjects = localStorage.getItem('objectDatabase');
+		objectDatabase = storedObjects ? JSON.parse(storedObjects) : [];
+	}
 
-    let results = objectDatabase;
+	function importCsv(e) {
+		const file = e.target.files[0];
+		if (file) {
+			Papa.parse(file, {
+				complete: function(results) {
+					objectDatabase = results.data.map(row => ({
+						id: row[0],
+						titel: row[1],
+						datumIn: row[2],
+						datumUit: row[3],
+						status: row[4]
+					}));
+					saveToLocalStorage();
+					displayObjects();
+				}
+			});
+		}
+	}
+	function downloadObjectAsCsv(objects) {
+    console.log("downloadObjectAsCsv is aangeroepen"); 
 
-    if (searchId) {
-        results = filterById(searchId);
-    }
+    const date = new Date();
+    const formattedDate = date.toISOString().split('T')[0];
+    const formattedTime = date.toTimeString().split(' ')[0].replace(/:/g, '');
+    const filename = `inuitobjecten_${formattedDate}_${formattedTime}.csv`;
+
+    let csvContent = "data:text/csv;charset=utf-8,";
+    csvContent += "ID,Titel,Datum In,Datum Uit,Status\n"; 
     
-    if (searchTitle) {
-        results = filterByTitle(searchTitle);
-    }
+    objects.forEach(obj => {
+        const row = `${obj.id},${obj.titel},${obj.datumIn},${obj.datumUit},${obj.status}`;
+        csvContent += row + "\r\n";
+    });
 
-    if (statusIn) {
-        results = filterByStatus('in');
-    } else if (statusUit) {
-        results = filterByStatus('uit');
-    }
-
-    displayObjects(results);
+    
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
 
 
-function clearSearch() {
-    document.getElementById('search-id').value = '';
-    document.getElementById('search-title').value = '';
-    document.getElementById('status-in').checked = false;
-    document.getElementById('status-uit').checked = false;
-    displayObjects(); // Toont alle objecten
+document.getElementById('export-csv-button').removeEventListener('click', handleExportCSVClick);
+
+function handleExportCSVClick() {
+    console.log("Knop 'export CSV' is geklikt"); 
+    downloadObjectAsCsv(objectDatabase);
 }
+
+document.getElementById('export-csv-button').addEventListener('click', handleExportCSVClick);
+
