9bee402b58e995d5d0cdaa82fb97ed3c3251a5b5
obanext <93115438+obanext@users.noreply.github.com>
2023-11-24 09:42:58 +0100

    Update lenlkaart.js

--- lenlkaart.js
+++ lenlkaart.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const baseMapUrl = 'https://localfocuswidgets.net/655f6ee99d02c?hide=dropdowns';
+  const baseMapUrl = 'https://localfocuswidgets.net/65603ea3927e4';
   const areaSelectors = ['ac', 'an', 'az', 'azo', 'anw', 'aw', 'ao', 'awe', 'ad'];
   const skillSelectors = ['t', 'd', 'g'];
 
@@ -25,24 +25,39 @@ document.addEventListener('DOMContentLoaded', function() {
     const selectedAreas = Array.from(document.querySelectorAll('#area-selectors input[type="checkbox"]:checked')).map(el => el.value);
     const selectedSkills = Array.from(document.querySelectorAll('#skill-selectors input[type="checkbox"]:checked')).map(el => el.value);
 
-    // Always include all areas
-    areaSelectors.forEach(area => {
-      url += `&activate|geonaam=${area}`;
-    });
-
-    // If skills are selected, append them, otherwise include all skills
-    if (selectedSkills.length > 0) {
-      selectedSkills.forEach(skill => {
-        areaSelectors.forEach(area => {
-          url += `&activate|selector=${area}${skill}`;
-        });
+    // If there are no selections made, then default to loading all areas and selectors
+    if (selectedAreas.length === 0 && selectedSkills.length === 0) {
+      areaSelectors.forEach(area => {
+        url += `&activate|geonaam=${area}&activate|selector=${area}`;
       });
     } else {
-      skillSelectors.forEach(skill => {
-        areaSelectors.forEach(area => {
-          url += `&activate|selector=${area}${skill}`;
-        });
+      // Add only the selected areas to the URL
+      selectedAreas.forEach(area => {
+        url += `&activate|geonaam=${area}`;
       });
+
+      // If skills are selected but no areas, add all areas with selected skills
+      if (selectedSkills.length > 0 && selectedAreas.length === 0) {
+        areaSelectors.forEach(area => {
+          selectedSkills.forEach(skill => {
+            url += `&activate|selector=${area}${skill}`;
+          });
+        });
+      }
+      // If areas are selected but no skills, add each selected area with its selector
+      else if (selectedSkills.length === 0 && selectedAreas.length > 0) {
+        selectedAreas.forEach(area => {
+          url += `&activate|selector=${area}`;
+        });
+      }
+      // If both areas and skills are selected, append them to the URL
+      else {
+        selectedSkills.forEach(skill => {
+          selectedAreas.forEach(area => {
+            url += `&activate|selector=${area}${skill}`;
+          });
+        });
+      }
     }
 
     // Update iframe source and display URL
@@ -54,6 +69,6 @@ document.addEventListener('DOMContentLoaded', function() {
   populateSelectors('area', areaSelectors, 'area-selectors');
   populateSelectors('skill', skillSelectors, 'skill-selectors');
 
-  // Initial update map call to load all areas and skills
+  // Call updateMap on page load to set the initial state with all areas and skills
   updateMap();
 });
