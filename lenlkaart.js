6c6fdae8d6661dc5ed6566603dd050d96a9e6759
obanext <93115438+obanext@users.noreply.github.com>
2023-11-30 14:07:04 +0100

    Update lenlkaart.js

--- lenlkaart.js
+++ lenlkaart.js
@@ -1,76 +1,83 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const baseMapUrl = 'https://localfocuswidgets.net/656828b5aa7c9?hide=dropdowns';
-  const areaSelectors = ['ac', 'an', 'az', 'azo', 'anw', 'aw', 'ao', 'awe', 'ad'];
-  const skillSelectors = ['t', 'd', 'g'];
+document.addEventListener('DOMContentLoaded', function () {
+    const baseMapUrl = 'https://localfocuswidgets.net/656828b5aa7c9?hide=dropdowns';
+    const areaSelectors = ['ac', 'an', 'az', 'azo', 'anw', 'aw', 'ao', 'awe', 'ad'];
+    const skillSelectors = ['T', 'd', 'g'];
 
-  function populateSelectors(selectorType, selectors, containerId) {
-    const container = document.getElementById(containerId);
-    selectors.forEach(selector => {
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.id = selector;
-      checkbox.value = selector;
-      checkbox.dataset.type = selectorType;
-      checkbox.addEventListener('change', updateMap); 
-      const label = document.createElement('label');
-      label.htmlFor = selector;
-      label.textContent = selector.toUpperCase();
-      container.appendChild(label);
-      container.appendChild(checkbox);
-    });
-  }
+    const areaLabels = {
+        ac: 'Centrum',
+        an: 'Noord',
+        az: 'Zuid',
+        azo: 'Zuidoost',
+        anw: 'Nieuw-West',
+        aw: 'West',
+        ao: 'Oost',
+        awe: 'Weesp',
+        ad: 'Diemen'
+    };
 
-  function updateMap() {
-    let url = baseMapUrl;
-    const selectedAreas = Array.from(document.querySelectorAll('#area-selectors input[type="checkbox"]:checked')).map(el => el.value);
-    const selectedSkills = Array.from(document.querySelectorAll('#skill-selectors input[type="checkbox"]:checked')).map(el => el.value);
+    const skillLabels = {
+        T: 'Taal',
+        d: 'Computer',
+        g: 'Geld en Rekenen'
+    };
 
-    
-    if (selectedAreas.length === 0 && selectedSkills.length === 0) {
-      areaSelectors.forEach(area => {
-        url += `&activate|geonaam=${area}&activate|selector=${area}`;
-      });
-    } else {
-     
-      selectedAreas.forEach(area => {
-        url += `&activate|geonaam=${area}`;
-      });
-
-      if (selectedSkills.length > 0 && selectedAreas.length === 0) {
-  areaSelectors.forEach(area => {
-   
-    url += `&activate|geonaam=${area}`;
-    selectedSkills.forEach(skill => {
-      
-      url += `&activate|selector=${area}${skill}`;
-    });
-  });
-}
-      
-      else if (selectedSkills.length === 0 && selectedAreas.length > 0) {
-        selectedAreas.forEach(area => {
-          url += `&activate|selector=${area}`;
+    function populateSelectors(selectorType, selectors, containerId, labels) {
+        const container = document.getElementById(containerId);
+        selectors.forEach(selector => {
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = selector;
+            checkbox.value = selector;
+            checkbox.dataset.type = selectorType;
+            checkbox.addEventListener('change', updateMap);
+            const label = document.createElement('label');
+            label.htmlFor = selector;
+            label.textContent = labels[selector];
+            container.appendChild(label);
+            container.appendChild(checkbox);
         });
-      }
-  
-      else {
-        selectedSkills.forEach(skill => {
-          selectedAreas.forEach(area => {
-            url += `&activate|selector=${area}${skill}`;
-          });
-        });
-      }
     }
 
-   
-    document.getElementById('map-frame').src = url;
-    document.getElementById('generated-url').textContent = url;
-  }
+    function updateMap() {
+        let url = baseMapUrl;
+        const selectedAreas = Array.from(document.querySelectorAll('#area-selectors input[type="checkbox"]:checked')).map(el => el.value);
+        const selectedSkills = Array.from(document.querySelectorAll('#skill-selectors input[type="checkbox"]:checked')).map(el => el.value);
 
-  
-  populateSelectors('area', areaSelectors, 'area-selectors');
-  populateSelectors('skill', skillSelectors, 'skill-selectors');
+        if (selectedAreas.length === 0 && selectedSkills.length === 0) {
+            areaSelectors.forEach(area => {
+                url += `&activate|geonaam=${area}&activate|selector=${area}`;
+            });
+        } else {
+            selectedAreas.forEach(area => {
+                url += `&activate|geonaam=${area}`;
+            });
 
-  
-  updateMap();
+            if (selectedSkills.length > 0 && selectedAreas.length === 0) {
+                areaSelectors.forEach(area => {
+                    url += `&activate|geonaam=${area}`;
+                    selectedSkills.forEach(skill => {
+                        url += `&activate|selector=${area}${skill}`;
+                    });
+                });
+            } else if (selectedSkills.length === 0 && selectedAreas.length > 0) {
+                selectedAreas.forEach(area => {
+                    url += `&activate|selector=${area}`;
+                });
+            } else {
+                selectedSkills.forEach(skill => {
+                    selectedAreas.forEach(area => {
+                        url += `&activate|selector=${area}${skill}`;
+                    });
+                });
+            }
+        }
+
+        document.getElementById('map-frame').src = url;
+        document.getElementById('generated-url').textContent = url;
+    }
+
+    populateSelectors('area', areaSelectors, 'area-selectors', areaLabels);
+    populateSelectors('skill', skillSelectors, 'skill-selectors', skillLabels);
+
+    updateMap();
 });
